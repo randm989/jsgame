0c238fb0873db111a5d35247af195ead4f92f011
randm989 <joshrutz989@gmail.com>
2013-10-22 01:02:36 -0700

    input working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 var WIDTH = 400;
 var HEIGHT = 300;
-var stage = new PIXI.Stage(0xEEFFFF);
+var mystage = new PIXI.Stage(0xEEFFFF, true);
 
 // let pixi choose WebGL or canvas
 var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
@@ -9,40 +9,46 @@ var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
 // attach render to page
 document.body.appendChild(renderer.view);
 
-var texture = PIXI.Texture.fromImage('img/leaf.png');
-var leaf = new PIXI.Sprite(texture);
+var texture = PIXI.Texture.fromImage('img/smiley.png');
+var player = new PIXI.Sprite(texture);
 
 // rotate around center
-leaf.anchor.x = 0.5;
-leaf.anchor.y = 0.5;
+player.anchor.x = 0.5;
+player.anchor.y = 0.5;
 
 
 
 // center in stage
-leaf.position.x = WIDTH / 2;
-leaf.position.y = HEIGHT / 2;
+player.position.x = WIDTH / 2;
+player.position.y = HEIGHT / 2;
 
 // place it on the stage for rendering
-stage.addChild(leaf);
+mystage.addChild(player);
 
 kd.LEFT.down(
     function()
     {
-        leaf.rotation += .02;
+        player.rotation += .02;
     }
 );
 
 kd.RIGHT.down(
     function()
     {
-        leaf.rotation -= .02;
+        player.rotation -= .02;
     }
-)
+);
+
+mystage.mousedown = function(e)
+{
+    player.position.x = e.global.x;
+    player.position.y = e.global.y;
+};
 
 function gameLoop() {
     requestAnimFrame(gameLoop);
-    renderer.render(stage);
+    renderer.render(mystage);
     kd.tick();
-}
+};
 
 requestAnimFrame(gameLoop);
